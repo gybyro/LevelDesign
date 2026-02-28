2232b0d821a04856f43145353c1c3b9603574aed
Gísli <148707024+Giblib07@users.noreply.github.com>
2026-02-28 12:29:55 +0000

    Lvl 1 - 3 polished. Lasers added to levels. Added Parallax Background
    Level 1 - 3 polished based on playtest feedback.
    Lasers added into levels replacing all saw objects.
    Added a parallax background.

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    private float startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
