b1cf2ef3bf69cf926b743ed37431c5f818e40b01
goji <100840760+gybyro@users.noreply.github.com>
2026-02-28 06:28:55 +0000

    new level

--- Assets/Scripts/CameraTarget.cs
+++ Assets/Scripts/CameraTarget.cs
@@ -10,14 +10,18 @@ namespace AGDDPlatformer
         void LateUpdate()
         {
             float averageX = 0;
+            float averagey = 0;
             foreach (Transform target in targets)
             {
                 averageX += target.position.x;
+                averagey += target.position.y;
             }
 
             averageX /= targets.Length;
+            averagey /= targets.Length;
 
-            transform.position = new Vector3(averageX, transform.position.y, transform.position.z);
+            // transform.position = new Vector3(averageX, transform.position.y, transform.position.z);
+            transform.position = new Vector3(averageX, averagey, transform.position.z);
         }
     }
 }
